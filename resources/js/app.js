e22ae218b96662d3d57c35b4bb5c603a52811dfb
Soushi888 <Sacha.pignot@gmail.com>
2020-07-17 23:16:04 -0400

    update

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import $ from "jquery";
+
+("use strict");
+
+// Génération du calendrier
+for (let i = 0; i <= 47; ++i) {
+    let tr = $("#modele").clone().attr("id", "");
+
+    let tdHeure = tr.children(".heure");
+
+    let heure = Math.floor(i / 2);
+    let tbody = $("tbody");
+
+    if (i % 2 == 0) {
+        tdHeure.text(`${heure}h00`);
+    } else {
+        tdHeure.text(`${heure}h30`);
+        tr.css("border-bottom", "1px solid black");
+    }
+    tdHeure.css("border-right", "1px solid black");
+
+    tbody.append(tr);
+}
--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -52,6 +52,8 @@
             </div>
         </div>
     </div>
+    {{-- <script src="http://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> --}}
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
