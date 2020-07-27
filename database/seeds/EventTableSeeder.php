3c2cd665b8d21237a595f9f4374271240f90fac4
Soushi888 <Sacha.pignot@gmail.com>
2020-07-27 04:25:25 -0400

    update

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -16,27 +16,27 @@ class EventTableSeeder extends Seeder
 
         $event = new Event;
         $event->name = "Cours de Javascript";
-        $event->date_debut = "2020-07-20 10:00:00";
-        $event->date_fin = "2020-07-20 12:00:00";
+        $event->date_debut = "2020-07-27 10:00:00";
+        $event->date_fin = "2020-07-27 12:00:00";
         $event->save();
 
         $event = new Event;
         $event->name = "Cours de Rust";
-        $event->date_debut = "2020-07-22 14:00:00";
-        $event->date_fin = "2020-07-22 16:00:00";
+        $event->date_debut = "2020-07-29 14:00:00";
+        $event->date_fin = "2020-07-29 16:00:00";
         $event->save();
 
         $event = new Event;
         $event->name = "Cours de Piano";
-        $event->date_debut = "2020-07-23 13:00:00";
-        $event->date_fin = "2020-07-23 14:00:00";
+        $event->date_debut = "2020-07-30 13:00:00";
+        $event->date_fin = "2020-07-30 14:00:00";
         $event->save();
 
         $event = new Event;
         $event->name = "Coffe and Code";
         $event->categorie = "réseautage";
-        $event->date_debut = "2020-07-24 08:00:00";
-        $event->date_fin = "2020-07-24 10:30:00";
+        $event->date_debut = "2020-07-31 08:00:00";
+        $event->date_fin = "2020-07-31 10:30:00";
         $event->save();
     }
 }
